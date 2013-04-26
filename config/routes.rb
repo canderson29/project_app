7b6ddecbf80bfaec521198e8e37fafed3518cce5
Colby <colby.anderson@eagles.oc.edu>
2013-04-25 21:31:35 -0500

    Competed Signin and site formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ProjectApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :testimonials
 
@@ -14,7 +15,10 @@ ProjectApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   match '/testimonials', to: 'testimonials#index'
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
