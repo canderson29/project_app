a82c6d917f70b7f392e64902a3bdf18d522f3b7d
Colby <colby.anderson@eagles.oc.edu>
2013-04-26 21:37:49 -0500

    Another CanCan Fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,9 @@ class Ability
   else
     can :read, :all;
     can :create, User;
+    can :update, User;
   end
+
     #
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
